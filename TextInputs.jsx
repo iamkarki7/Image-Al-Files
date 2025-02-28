80699bb40b33cad1af74173c21226b8201c28501
sanjay karki <84610328+iamkarki7@users.noreply.github.com>
2025-02-28 23:34:51 +0200

    file

--- TextInputs.jsx
+++ TextInputs.jsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 const Container = styled.div`
   flex: 1;
   display: flex;
+  display: flex;
+  display: flex;
+  display: flex;
   flex-direction: column;
   gap: 4px;
 `;
